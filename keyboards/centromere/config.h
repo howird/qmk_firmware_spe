52616135da4450f666fb3235a632cfb2d7938e28
Howard nguyen-Huu <howardnguyenhuu@gmail.com>
2021-07-26 21:09:06 -0400

    added keymap

--- keyboards/centromere/config.h
+++ keyboards/centromere/config.h
@@ -22,11 +22,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define VENDOR_ID       0x2001
+#define PRODUCT_ID      0x0125
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    SouthpawDesign
-#define PRODUCT         centromere
+#define PRODUCT         howirdcentromere
 #define DESCRIPTION     q.m.k. keyboard firmware for centromere
 
 /* key matrix size */
